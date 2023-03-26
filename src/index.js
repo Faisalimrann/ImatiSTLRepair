7c58352059d7b560bd1a29c1add729254fbf194c
Faisal Imran <faisal.imran@arbisoft.com>
2023-03-26 13:04:17 +0500

    - Added error handling

--- src/index.js
+++ src/index.js
@@ -4,18 +4,33 @@ const os = require('os');
 
 const ImatiSTL_COMMAND =
   os.platform() === 'darwin'
-    ? console.warn('This package is not supported on macOS')
+    ? undefined
     : path.join(__dirname, '..', 'libs/Windows/ImatiSTL/MeshFix.exe');
 
-async function repair(inputputFilePath, outputputFilePath) {
+function validateFilePath(filePath) {
+  if (!fs.existsSync(filePath)) {
+    throw new Error(`Invalid file path: ${filePath}`);
+  }
+}
+
+function buildRepairCommand(inputFilePath, outputFilePath) {
+  if (!ImatiSTL_COMMAND) console.warn('This package is not supported on macOS');
+  return `${ImatiSTL_COMMAND} "${inputFilePath}" "${outputFilePath}"`;
+}
+
+async function repair(inputFilePath, outputFilePath) {
+  validateFilePath(inputFilePath);
+
+  const command = buildRepairCommand(inputFilePath, outputFilePath);
+
   return new Promise((resolve, reject) => {
-    shell.exec(
-      `${ImatiSTL_COMMAND} "${inputputFilePath}" "${outputputFilePath}"`,
-      (code, output, error) => {
-        if (error) return reject({ code, output, error });
-        return resolve(`${outputputFilePath}`);
-      },
-    );
+    shell.exec(command, (code, output, error) => {
+      if (error) {
+        reject(new Error(`Failed to repair file: ${error}`));
+      } else {
+        resolve(outputFilePath);
+      }
+    });
   });
 }
 
